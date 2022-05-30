a90d3e1d67baa867e582e862eba985377c51562d
kkt4828 <kkt4828@kakao.com>
2022-05-30 14:43:41 +0000

    수정2

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - Docker Build Deploy(Modified)")
+    st.title("Mask Classification Model - Docker Build Deploy(Modified2)")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
